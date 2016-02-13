1971fd8a0397d4d01e8a7bce05349fb36842c337
Phillip Laclede <prlaclede@gmail.com>
2016-02-13 18:53:18 +0000

    js bug fixes

--- static/js/custom/email.js
+++ static/js/custom/email.js
@@ -1,20 +1,31 @@
 $(function (email, $, undefined) {
     
     $('body')
-                .on('click', '#parseEmails', function() {
-                    var emailList = $('#emailList').val();
-                    var emailArray = email.parseEmails(emailList);
-                    $.each(emailArray, function(i) {
-                        $('#parsedEmails').append(emailArray[i] + '\n');
-                    });
-                })
-                
-                .on('click', '#clearEmails', function() {
-                    $('#parsedEmails').empty();
-                })
+            .on('click', '#parseEmails', function() {
+                var emailList = $('#emailList').val();
+                var emailArray = email.parseEmails(emailList);
+                $.each(emailArray, function(i) {
+                    if (!email.isValidEmailAddress(emailArray[i])) {
+                        $('#parsedEmails').append(emailArray[i] + " error" + '\n');
+                    } else {
+                        $('#parsedEmails').append(emailArray[i] + '\n');   
+                    }
+                });
+                $('#emailList').val('');
+            })
+            
+            .on('click', '#clearEmails', function() {
+                $('#parsedEmails').val('');
+            })
     
     email.parseEmails = function(emailList) {
         return emailList.split(',');
+    }
+    
+    email.isValidEmailAddress = function (emailAddress) {
+        /* from the regex library at https://regex101.com/ */
+        var pattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return pattern.test(emailAddress);
     }
     
 }( window.email = window.email || {}, jQuery ));
